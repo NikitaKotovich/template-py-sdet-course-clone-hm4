333770d6cdc82d7e998b66fb9023b4abd46bfa78
NikitaKotovich <nikita.kotovich28@gmail.com>
2025-09-04 20:40:36 +0300

    change in hw5_solution.py

--- homeworks/hw5/hw5_solution.py
+++ homeworks/hw5/hw5_solution.py
@@ -1,38 +1,50 @@
+
 def add_ing(s: str) -> str:
-    pass
+    s += 'ing'
+    return s
 
 
 def change_symbol(s: str) -> str:
-    pass
+    s = s.replace('#', '/')
+    return s
 
 
 def change_order(s: str) -> str:
-    pass
+    name = s.split()
+    return ' '.join(reversed(name))
 
 
 def clean_string(s: str) -> str:
-    pass
+    s = s.strip()
+    return s
 
 
 def to_capitalize(s: str) -> str:
-    pass
+    s = s.capitalize()
+    return s
 
 
 def to_list(s: str) -> list:
-    pass
+    arr = s.split()
+    return arr
 
 
 def formatting(array: list, s1: str, s2: str) -> str:
-    pass
+    a = " ".join(array)
+    b = "Hello, {a}! {s1} to {s2}".format(a=a, s1=s1, s2=s2)
+    return b
 
 
 def to_string(array: list) -> str:
-    pass
+    arr = " ".join(array)
+    return arr
 
 
 def insert_to_list(array: list, item: int | str, indx: int) -> list:
-    pass
+    array.insert(indx, item)
+    return array
 
 
 def delete_from_list(array: list, indx: int) -> list:
-    pass
+    del array[indx]
+    return array
