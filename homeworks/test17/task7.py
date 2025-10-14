9e5771991c262f46d3d0c1b7f0263b2fda090694
NikitaKotovich <nikita.kotovich28@gmail.com>
2025-10-14 20:42:00 +0300

    task7 solution, Kotovich Nikita

---
+++ homeworks/test17/task7.py
@@ -0,0 +1,8 @@
+def change_str(s, n):
+    a = s[:n]
+    return a + a[-2::-1]
+s = "abcdef...xyz"
+assert change_str(s, 1) == "a"
+assert change_str(s, 2) == "aba"
+assert change_str(s, 3) == "abcba"
+assert change_str(s, 4) == "abcdcba"
